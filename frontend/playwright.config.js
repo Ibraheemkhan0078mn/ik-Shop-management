e0da0c6e97a97970836fc7f0fcacf1971cc50c0a
Ibraheemkhan0078mn <ikkhan007m@gmail.com>
2026-07-01 09:29:10 +0500

    configuring playwrite

---
+++ frontend/playwright.config.js
@@ -0,0 +1,24 @@
+const { defineConfig, devices } = require('@playwright/test');
+
+module.exports = defineConfig({
+  testDir: './src',
+  testMatch: '**/test/*.test.js',
+  fullyParallel: false,
+  workers: 1,
+  forbidOnly: !!process.env.CI,
+  retries: process.env.CI ? 2 : 0,
+  // workers: process.env.CI ? 1 : undefined,
+  reporter: 'html',
+  use: {
+    baseURL: 'http://localhost:5173',
+    trace: 'on-first-retry',
+  },
+  projects: [
+    { name: 'chromium', use: { ...devices['Desktop Chrome'] } },
+  ],
+  webServer: {
+    command: 'npm run dev',
+    url: 'http://localhost:5173',
+    reuseExistingServer: !process.env.CI,
+  },
+});
