f9ff43e500a6d2afb07c567ce3e50f3892f02bf0
Ibraheemkhan0078mn <ikkhan007m@gmail.com>
2026-08-09 09:51:02 +0500

    permission changed dleeet funtion logica nd sync organzer filteration is correct.

--- backend/modules/backup/services/permissionChangeDeletion.js
+++ backend/modules/backup/services/permissionChangeDeletion.js
@@ -6,65 +6,28 @@
 
 export async function permissionChangedDeletionFromLocal(modelArray, loggedInUserData) {
     try {
-
-        let userPermissions = loggedInUserData?.permissions
-
-        for (let eachModelCollection of modelArray) {
-            if (userPermissions && userPermissions?.length > 0) {
-
-
-
-
-                // when not admin. Then he need only his own data in this local storage. so delete other when not admin
-                if (eachModelCollection.local.modelName == "user" && loggedInUserData.role != "admin") {
-                    await eachModelCollection?.local?.deleteMany({ _id: { $ne: loggedInUserData?._id } })
-                }
-
-
-                // every model in my database have some permission string and when this permission string is with user then he allowed otherwise it delete the entire model collection from the local db
-                let permissionCheckresult = eachModelCollection.permissionString.some(permission => userPermissions?.includes(permission))
-                if (!permissionCheckresult &&
-                    loggedInUserData?.role != "admin" &&
-                    eachModelCollection.local.modelName != "user"
-                ) {
-                    await eachModelCollection?.local?.collection?.drop()
-                }
-
-
-
-
-
-                // when loop is running for class then it delete the classes also from local which are not permitted to user. means permistion is changed and new permition these classes are nto allowed
-                let allowedClasses = loggedInUserData?.allowedClases
-                if (loggedInUserData && eachModelCollection?.local?.modelName == "class") {
-                    if (allowedClasses?.length > 0 && !(loggedInUserData?.role == "admin")) {
-                        await eachModelCollection?.local?.deleteMany({
-                            _id: { $nin: allowedClasses }
-                        });
-                    }
-                }
-
-
-
-
-
-                // students related with allowed class only remained other is deleted when class permition is changed by admin.
-                if (loggedInUserData && eachModelCollection?.local?.modelName == "student") {
-                    if (allowedClasses?.length > 0 && loggedInUserData?.role != "admin") {
-                        await eachModelCollection?.local?.deleteMany({
-                            classId: { $nin: allowedClasses }
-                        });
-                    }
-                }
-
-
-
-
-            }
+        // 1: If loggedinUserData is admin then return and do nothing
+        if (loggedInUserData?.role === "admin") {
+            return;
         }
 
+        let userPermissions = loggedInUserData?.permissions;
 
+        for (let eachModelCollection of modelArray) {
+            // 2: When not admin, in local instead of his data on this _id, delete all the user from this local
+            if (eachModelCollection.local.modelName === "user") {
+                await eachModelCollection?.local?.deleteMany({ _id: { $ne: loggedInUserData?._id } });
+                continue;
+            }
+
+            // 3: Check its permission string and which permission is not given to him then delete the document of this model
+            // and remains only document of models which permission is given to this user
+            let permissionCheckResult = eachModelCollection.permissionString.some(permission => userPermissions?.includes(permission));
+            if (!permissionCheckResult) {
+                await eachModelCollection?.local?.deleteMany({});
+            }
+        }
     } catch (error) {
-        throw new Error(error?.message)
+        throw new Error(error?.message);
     }
 }
--- backend/modules/backup/services/syncOrganizedRunner.js
+++ backend/modules/backup/services/syncOrganizedRunner.js
@@ -1,5 +1,5 @@
-import { getLocalActivityLogModel, getLocalExpenseCategoryModel, getLocalExpensesModel, getLocalQarzaAccountModel, getLocalQarzaPaymentModel, getLocalUserModel, getLocalProductModel, getLocalCategoryModel, getLocalSubCategoryModel, getLocalBatchModel, getLocalSupplierModel, getLocalPurchaseModel, getLocalPurchasePaymentModel, getLocalOrderModel, getLocalHoldOrderModel, getLocalWastageModel, getLocalPurchaseReturnModel, getLocalProductReturnModel, getLocalCustomerModel, getLocalStaffModel, getLocalStaffSalaryPaymentModel, getLocalStaffSaleBillModel, getLocalStaffAttendanceModel, getLocalSettingsModel, getLocalPaymentMethodModel, getLocalAppThemeModel } from "../../../configs/connect.db.js"
-import { getOnlineActivityLogModel, getOnlineExpenseCategoryModel, getOnlineExpensesModel, getOnlineQarzaAccountModel, getOnlineQarzaPaymentModel, getOnlineUserModel, getOnlineProductModel, getOnlineCategoryModel, getOnlineSubCategoryModel, getOnlineBatchModel, getOnlineSupplierModel, getOnlinePurchaseModel, getOnlinePurchasePaymentModel, getOnlineOrderModel, getOnlineHoldOrderModel, getOnlineWastageModel, getOnlinePurchaseReturnModel, getOnlineProductReturnModel, getOnlineCustomerModel, getOnlineStaffModel, getOnlineStaffSalaryPaymentModel, getOnlineStaffSaleBillModel, getOnlineStaffAttendanceModel, getOnlineSettingsModel, getOnlinePaymentMethodModel } from '../../../configs/onlineConnect.db.js'
+import { getLocalActivityLogModel, getLocalExpenseCategoryModel, getLocalExpensesModel, getLocalQarzaAccountModel, getLocalQarzaPaymentModel, getLocalUserModel, getLocalProductModel, getLocalCategoryModel, getLocalSubCategoryModel, getLocalBatchModel, getLocalSupplierModel, getLocalPurchaseModel, getLocalPurchasePaymentModel, getLocalOrderModel, getLocalHoldOrderModel, getLocalWastageModel, getLocalPurchaseReturnModel, getLocalProductReturnModel, getLocalCustomerModel, getLocalStaffModel, getLocalStaffSalaryPaymentModel, getLocalStaffSaleBillModel, getLocalStaffAttendanceModel, getLocalSettingsModel, getLocalPaymentMethodModel, getLocalAppThemeModel, getLocalBrandModel, getLocalUserRoleModel, getLocalStaffRoleModel } from "../../../configs/connect.db.js"
+import { getOnlineActivityLogModel, getOnlineExpenseCategoryModel, getOnlineExpensesModel, getOnlineQarzaAccountModel, getOnlineQarzaPaymentModel, getOnlineUserModel, getOnlineProductModel, getOnlineCategoryModel, getOnlineSubCategoryModel, getOnlineBatchModel, getOnlineSupplierModel, getOnlinePurchaseModel, getOnlinePurchasePaymentModel, getOnlineOrderModel, getOnlineHoldOrderModel, getOnlineWastageModel, getOnlinePurchaseReturnModel, getOnlineProductReturnModel, getOnlineCustomerModel, getOnlineStaffModel, getOnlineStaffSalaryPaymentModel, getOnlineStaffSaleBillModel, getOnlineStaffAttendanceModel, getOnlineSettingsModel, getOnlinePaymentMethodModel, getOnlineAppThemeModel, getOnlineBrandModel, getOnlineUserRoleModel, getOnlineStaffRoleModel } from '../../../configs/onlineConnect.db.js'
 import { deleteOnlineSync } from "./deleteOnlineSync.js";
 import { downloadOnlineSync } from "./downloadOnlineSync.js";
 import { permissionChangedDeletionFromLocal } from "./permissionChangeDeletion.js";
@@ -20,31 +20,35 @@ export async function docsSyncOrganizer(syncType = "required", loggedInUserData)
         console.log("The sync organizer is starts")
 
         let modelArray = [
-            { local: getLocalProductModel(), online: getOnlineProductModel(), permissionString: ["products-view"] },
-            { local: getLocalCategoryModel(), online: getOnlineCategoryModel(), permissionString: ["categories-view"] },
-            { local: getLocalSubCategoryModel(), online: getOnlineSubCategoryModel(), permissionString: ["subcategories-view"] },
-            { local: getLocalBatchModel(), online: getOnlineBatchModel(), permissionString: ["batches-view"] },
-            { local: getLocalSupplierModel(), online: getOnlineSupplierModel(), permissionString: ["suppliers-view"] },
-            { local: getLocalPurchaseModel(), online: getOnlinePurchaseModel(), permissionString: ["purchases-view"] },
-            { local: getLocalPurchasePaymentModel(), online: getOnlinePurchasePaymentModel(), permissionString: ["purchase-payments-view"] },
-            { local: getLocalOrderModel(), online: getOnlineOrderModel(), permissionString: ["orders-view"] },
-            { local: getLocalHoldOrderModel(), online: getOnlineHoldOrderModel(), permissionString: ["hold-orders-view"] },
-            { local: getLocalExpensesModel(), online: getOnlineExpensesModel(), permissionString: ["expenses-view"] },
-            { local: getLocalExpenseCategoryModel(), online: getOnlineExpenseCategoryModel(), permissionString: ["expense-category-view"] },
-            { local: getLocalQarzaAccountModel(), online: getOnlineQarzaAccountModel(), permissionString: ["qarzas-with-account-view"] },
-            { local: getLocalQarzaPaymentModel(), online: getOnlineQarzaPaymentModel(), permissionString: ["qarza-with-account-payment-view"] },
-            { local: getLocalWastageModel(), online: getOnlineWastageModel(), permissionString: ["wastage-view"] },
-            { local: getLocalPurchaseReturnModel(), online: getOnlinePurchaseReturnModel(), permissionString: ["purchase-returns-view"] },
-            { local: getLocalProductReturnModel(), online: getOnlineProductReturnModel(), permissionString: ["product-returns-view"] },
-            { local: getLocalCustomerModel(), online: getOnlineCustomerModel(), permissionString: ["customers-view"] },
-            { local: getLocalStaffModel(), online: getOnlineStaffModel(), permissionString: ["staff-view"] },
-            { local: getLocalStaffSalaryPaymentModel(), online: getOnlineStaffSalaryPaymentModel(), permissionString: ["staff-salary-payments-view"] },
-            { local: getLocalStaffSaleBillModel(), online: getOnlineStaffSaleBillModel(), permissionString: ["staff-sale-bills-view"] },
-            { local: getLocalStaffAttendanceModel(), online: getOnlineStaffAttendanceModel(), permissionString: ["staff-attendance-view"] },
-            { local: getLocalUserModel(), online: getOnlineUserModel(), permissionString: ["users-view"] },
-            { local: getLocalActivityLogModel(), online: getOnlineActivityLogModel(), permissionString: ["activityLogs-view"] },
-            { local: getLocalSettingsModel(), online: getOnlineSettingsModel(), permissionString: ["settings-view"] },
-            { local: getLocalPaymentMethodModel(), online: getOnlinePaymentMethodModel(), permissionString: ["payment-methods-view"] }
+            { local: getLocalProductModel(), online: getOnlineProductModel(), permissionString: ["products.view"] },
+            { local: getLocalCategoryModel(), online: getOnlineCategoryModel(), permissionString: ["categories.view"] },
+            { local: getLocalSubCategoryModel(), online: getOnlineSubCategoryModel(), permissionString: ["subcategories.view"] },
+            { local: getLocalBrandModel(), online: getOnlineBrandModel(), permissionString: ["brands.view"] },
+            { local: getLocalBatchModel(), online: getOnlineBatchModel(), permissionString: ["batches.view"] },
+            { local: getLocalSupplierModel(), online: getOnlineSupplierModel(), permissionString: ["suppliers.view"] },
+            { local: getLocalPurchaseModel(), online: getOnlinePurchaseModel(), permissionString: ["purchases.view"] },
+            { local: getLocalPurchasePaymentModel(), online: getOnlinePurchasePaymentModel(), permissionString: ["purchases.view"] },
+            { local: getLocalOrderModel(), online: getOnlineOrderModel(), permissionString: ["orders.view"] },
+            { local: getLocalHoldOrderModel(), online: getOnlineHoldOrderModel(), permissionString: ["pos.orders.hold"] },
+            { local: getLocalExpensesModel(), online: getOnlineExpensesModel(), permissionString: ["expenses.view"] },
+            { local: getLocalExpenseCategoryModel(), online: getOnlineExpenseCategoryModel(), permissionString: ["expenses.view"] },
+            { local: getLocalQarzaAccountModel(), online: getOnlineQarzaAccountModel(), permissionString: ["creditsAndDebitsAccounts.view"] },
+            { local: getLocalQarzaPaymentModel(), online: getOnlineQarzaPaymentModel(), permissionString: ["creditsDebits.view"] },
+            { local: getLocalWastageModel(), online: getOnlineWastageModel(), permissionString: ["wastage.view"] },
+            { local: getLocalPurchaseReturnModel(), online: getOnlinePurchaseReturnModel(), permissionString: ["purchaseReturns.view"] },
+            { local: getLocalProductReturnModel(), online: getOnlineProductReturnModel(), permissionString: ["productReturns.view"] },
+            { local: getLocalCustomerModel(), online: getOnlineCustomerModel(), permissionString: ["customers.view"] },
+            { local: getLocalStaffModel(), online: getOnlineStaffModel(), permissionString: ["staff.view"] },
+            { local: getLocalStaffRoleModel(), online: getOnlineStaffRoleModel(), permissionString: ["staff.view"] },
+            { local: getLocalStaffSalaryPaymentModel(), online: getOnlineStaffSalaryPaymentModel(), permissionString: ["staff.salaries.view"] },
+            { local: getLocalStaffSaleBillModel(), online: getOnlineStaffSaleBillModel(), permissionString: ["staff.orders.view"] },
+            { local: getLocalStaffAttendanceModel(), online: getOnlineStaffAttendanceModel(), permissionString: ["staff.view"] },
+            { local: getLocalUserModel(), online: getOnlineUserModel(), permissionString: ["users.view"] },
+            { local: getLocalUserRoleModel(), online: getOnlineUserRoleModel(), permissionString: ["userRoles.view"] },
+            { local: getLocalActivityLogModel(), online: getOnlineActivityLogModel(), permissionString: ["dashboard.view"] },
+            { local: getLocalSettingsModel(), online: getOnlineSettingsModel(), permissionString: ["settings.view"] },
+            { local: getLocalPaymentMethodModel(), online: getOnlinePaymentMethodModel(), permissionString: ["settings.paymentMethods"] },
+            { local: getLocalAppThemeModel(), online: getOnlineAppThemeModel(), permissionString: ["settings.theme"] }
         ];
 
 
@@ -60,24 +64,19 @@ export async function docsSyncOrganizer(syncType = "required", loggedInUserData)
 
         // then check if someone have not user permission then filter it reduce load on others
         // IDEA: what if i filter the all the modesl here so the load is reduce on other utilities of it.
-        if (loggedInUserData.role != "admin" && (!loggedInUserData.permissions?.includes("users-view"))) {
-            modelArray.filter(m => m.local.modelName != "user")
-        }
-
-        // console.log(loggedInUserData.role, loggedInUserData.permissions)
         if (loggedInUserData.role != "admin") {
             modelArray = modelArray.filter(mObject => {
-                // console.log(loggedInUserData.permissions?.includes(mObject.permissionString), mObject.permissionString, 'filter')
-                if (loggedInUserData.permissions?.includes(mObject.permissionString)) {
-                    return true;
-                }
+                // Check if user has any of the required permissions for this model
+                const hasPermission = mObject.permissionString.some(permission => 
+                    loggedInUserData.permissions?.includes(permission)
+                );
+                return hasPermission;
             })
-            // modelArray.forEach(m => console.log(m.permissionString))
         }
 
 
 
-        await deleteOnlineSync(modelArray, loggedInUserData)
+        // await deleteOnlineSync(modelArray, loggedInUserData)  // do not remove this comment
         await onlineDocsUploadSyncInsert(modelArray, syncType, loggedInUserData)
         await onlineDocsUploadSyncUpdate(modelArray, syncType, loggedInUserData)
         await downloadOnlineSync(modelArray, syncType, loggedInUserData)
